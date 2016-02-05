80661ef81681645e1dc90dd169ac55d25b6ab39f
Edmundo Pichardo <ce.pichardo@gmail.com>
2016-02-05 19:35:25 -0400

    Listo parasubir

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204023558) do
+ActiveRecord::Schema.define(version: 20160205202055) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 20160204023558) do
   end
 
   add_index "movies", ["user_id"], name: "index_movies_on_user_id"
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
